157583044dbca6dae5d63f49ad74560ee0eba557
akshit <akshits987@gmail.com>
2024-08-16 17:58:03 +0530

    updates

--- src/components/Verifications.tsx
+++ src/components/Verifications.tsx
@@ -8,9 +8,26 @@ interface Props {
 
 export const Verifications: React.FC<Props> = ({ verifications, onChange }) => {
   const handleChange = (vIndex: number, cIndex: number, value: boolean) => {
-    const v = structuredClone(verifications);
-    
-    v[vIndex].eligibility.checks[cIndex].passed = value;
+    // const v = structuredClone(verifications);
+
+    // v[vIndex].eligibility.checks[cIndex].passed = value;
+
+    ///------updates------------------
+    const v = verifications.map((verification, index) => {
+      if (index !== vIndex) return verification;
+
+      return {
+        ...verification,
+        eligibility: {
+          ...verification.eligibility,
+          checks: verification.eligibility.checks.map((iCheck, cidx) => {
+            if (cidx !== cIndex) return iCheck;
+            return { ...iCheck, passed: value };
+          }),
+        },
+      };
+    });
+    ///------updates------------------
     onChange(v);
   };
 
@@ -25,12 +42,12 @@ export const Verifications: React.FC<Props> = ({ verifications, onChange }) => {
             </div>
             <div className="flex flex-wrap">
               {verification.eligibility.checks.map((condition, cIndex) => {
+                ///------updates------------------
+                const key = `${verification.source}_${condition.name}_${cIndex}`;
+                ///------updates------------------
                 return (
-                  <div className="p-px w-1/2" key={condition.name}>
-                    <div
-                      key={condition.name}
-                      className="flex flex-col bg-white p-4 rounded-lg"
-                    >
+                  <div className="p-px w-1/2" key={key}>
+                    <div className="flex flex-col bg-white p-4 rounded-lg">
                       <p className="text-sm font-medium text-neutral-700 mb-1 -mt-1">
                         {condition.label}
                       </p>
